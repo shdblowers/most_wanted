518f0af5ab4f410e5236d4e1ce723f6a4a0af94f
Steven Blowers <shdblowers@gmail.com>
2016-09-24 16:49:35 +0100

    Revert "new page for requests"
    This reverts commit 2ce97b457654d3af87fd428e75bd37af099b6a59.

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,6 @@ defmodule MostWanted.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/requests", RequestController, :index
   end
 
   # Other scopes may use custom stacks.
